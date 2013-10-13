139799d074b43a3a7ca5ae52bd32c6e1e2d7c143
Globalcaching <hemenrob@gmail.com>
2013-10-13 17:37:04 +0200

    - add: OKAPI, Import bij list of geocache codes

---
+++ DefaultPlugins/GlobalcachingApplication.Plugins.OKAPI/ImportByGCCodes.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GlobalcachingApplication.Plugins.OKAPI
+{
+    public class ImportByGCCodes : Utils.BasePlugin.BaseImportFilter
+    {
+        public const string STR_IMPORTINGCACHES = "Importing opencaching geocaches...";
+        public const string STR_ERROR = "Error";
+
+        public const string ACTION_IMPORT = "Import geocaches by codes";
+
+        private string _errormessage = null;
+        private List<string> _gcList = null;
+
+        public override bool Initialize(Framework.Interfaces.ICore core)
+        {
+            AddAction(ACTION_IMPORT);
+
+            core.LanguageItems.Add(new Framework.Data.LanguageItem(STR_IMPORTINGCACHES));
+            core.LanguageItems.Add(new Framework.Data.LanguageItem(STR_ERROR));
+
+            core.LanguageItems.Add(new Framework.Data.LanguageItem(ImportByGCCodesForm.STR_TITLE));
+            core.LanguageItems.Add(new Framework.Data.LanguageItem(ImportByGCCodesForm.STR_LIST));
+            core.LanguageItems.Add(new Framework.Data.LanguageItem(ImportByGCCodesForm.STR_OK));
+
+            return base.Initialize(core);
+        }
+
+        public override string DefaultAction
+        {
+            get
+            {
+                return ACTION_IMPORT;
+            }
+        }
+
+        public override Framework.PluginType PluginType
+        {
+            get
+            {
+                return Framework.PluginType.OKAPI;
+            }
+        }
+
+        protected override void ImportMethod()
+        {
+            try
+            {
+                using (Utils.ProgressBlock progress = new Utils.ProgressBlock(this, STR_IMPORTINGCACHES, STR_IMPORTINGCACHES, _gcList.Count, 0, true))
+                {
+                    int gcupdatecount = 1;
+                    int max = _gcList.Count;
+                    while (_gcList.Count > 0)
+                    {
+                        List<string> lcs = _gcList.Take(gcupdatecount).ToList();
+                        _gcList.RemoveRange(0, lcs.Count);
+                        List<OKAPIService.Geocache> caches = OKAPIService.GetGeocaches(SiteManager.Instance.ActiveSite, lcs);
+                        Import.AddGeocaches(Core, caches);
+
+                        if (!progress.UpdateProgress(STR_IMPORTINGCACHES, STR_IMPORTINGCACHES, max, max - _gcList.Count))
+                        {
+                            break;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                _errormessage = e.Message;
+            }
+        }
+
+        public override bool Action(string action)
+        {
+            bool result = base.Action(action);
+            if (result && action == ACTION_IMPORT)
+            {
+                if (SiteManager.Instance.CheckAPIAccess())
+                {
+                    using (ImportByGCCodesForm dlg = new ImportByGCCodesForm())
+                    {
+                        if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                        {
+                            _gcList = dlg.GCCodes;
+                            PerformImport();
+                            if (!string.IsNullOrEmpty(_errormessage))
+                            {
+                                System.Windows.Forms.MessageBox.Show(_errormessage, Utils.LanguageSupport.Instance.GetTranslation(Utils.LanguageSupport.Instance.GetTranslation(STR_ERROR)), System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ DefaultPlugins/GlobalcachingApplication.Plugins.OKAPI/ImportByGCCodesForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GlobalcachingApplication.Plugins.OKAPI
+{
+    public partial class ImportByGCCodesForm : Form
+    {
+        public const string STR_TITLE = "Import geocaches by codes";
+        public const string STR_LIST = "Provide list of geocache codes";
+        public const string STR_OK = "OK";
+
+        public ImportByGCCodesForm()
+        {
+            InitializeComponent();
+
+            this.Text = Utils.LanguageSupport.Instance.GetTranslation(STR_TITLE);
+            this.label1.Text = Utils.LanguageSupport.Instance.GetTranslation(STR_LIST);
+            this.button1.Text = Utils.LanguageSupport.Instance.GetTranslation(STR_OK);
+        }
+
+        public List<string> GCCodes
+        {
+            get
+            {
+                List<string> result = new List<string>();
+                string[] parts = textBox1.Text.Split(new char[] {' ', ',','.','\r','\n','\t',';' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in parts)
+                {
+                    result.Add(s.ToUpper());
+                }
+                return result;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+            Close();
+        }
+    }
+}
