3edc92ec776bb58987aade209bf5f2ae4d0f6ca2
Globalcaching <hemenrob@gmail.com>
2013-11-27 20:31:48 +0100

    gappsf development

---
+++ GAPPSF/UIControls/OfflineImages/OfflineImagesManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAPPSF.UIControls.OfflineImages
+{
+    public class OfflineImagesManager
+    {
+        private static OfflineImagesManager _uniqueInstance = null;
+        private static object _lockObject = new object();
+
+        private OfflineImagesManager()
+        {
+
+        }
+
+        public OfflineImagesManager Instance
+        {
+            get
+            {
+                if (_uniqueInstance == null)
+                {
+                    lock (_lockObject)
+                    {
+                        if (_uniqueInstance==null)
+                        {
+                            _uniqueInstance = new OfflineImagesManager();
+                        }
+                    }
+                }
+                return _uniqueInstance;
+            }
+        }
+    }
+}
